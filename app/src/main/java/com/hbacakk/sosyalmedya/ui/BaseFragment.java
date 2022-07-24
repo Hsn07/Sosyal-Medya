f414e3dad75196f2021f31175534e277edbc4c73
Hsn07 <hbacak15@posta.pau.edu.tr>
2022-07-25 00:45:02 +0300

    Add Base Fragment
    Created base fragment

---
+++ app/src/main/java/com/hbacakk/sosyalmedya/ui/BaseFragment.java
@@ -0,0 +1,33 @@
+package com.hbacakk.sosyalmedya.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment<T extends ViewDataBinding> extends Fragment {
+
+    public T binding;
+    public abstract int getLayoutResId();
+
+    @CallSuper
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = (T) DataBindingUtil.inflate(LayoutInflater.from(getContext()), getLayoutResId(), container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
